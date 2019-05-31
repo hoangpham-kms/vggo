a37300d50f2a1965f492332d1fc2a04e5a9ff64d
phamvanhoang1510 <phamvanhoang1510@gmail.com>
2019-05-31 11:59:09 +0700

    Enabled routing and exposed components for using in routing.

--- projects/vg-lib/src/public-api.ts
+++ projects/vg-lib/src/public-api.ts
@@ -4,3 +4,7 @@
 export * from './lib/vg-lib.service';
 export * from './lib/vg-lib.component';
 export * from './lib/vg-lib.module';
+export * from './lib/login/login.component';
+export * from './lib/header/header.component';
+export * from './lib/footer/footer.component';
+export * from './lib/leftsidebar/leftsidebar.component';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { VgLibModule } from 'vg-lib';
-
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 @ NgModule({
   declarations: [
-    AppComponent,
+    AppComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    VgLibModule
+    VgLibModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
